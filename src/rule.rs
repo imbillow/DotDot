d314b137511d23c1c409044f8afd82a19e25daa2
billow <billow.fun@gmail.com>
2020-03-20 13:49:31 +0800

    Maybe Compromised, using the trite Tree to be the  PathTree?

--- src/rule.rs
+++ src/rule.rs
@@ -1,9 +1,16 @@
+use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::fs;
 use std::path::Path;
 
+#[derive(Deserialize, Serialize, Debug)]
+struct Tree {
+    current: String,
+    children: Option<Vec<Tree>>,
+}
+
 pub fn resolve(path: &Path) {
     let content = fs::read_to_string(path).unwrap();
-    let yml: BTreeMap<String, String> = serde_yaml::from_str(&content).unwrap();
+    let yml: Tree = serde_yaml::from_str(&content).unwrap();
     println!("{:?}", yml)
 }
