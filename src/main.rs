093bc10f62985c54c6663beb83b294774f0877e2
billow <billow.fun@gmail.com>
2020-03-20 10:00:29 +0800

    It just can run cli(Command Line Interface)

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,44 @@
-use std::{env, fs};
 use std::error::Error;
 use std::path::Path;
+use std::{env, fs};
 
+use clap::{App, Arg, ArgMatches};
 use serde::{Deserialize, Serialize};
-use clap::{App, ArgMatches, Arg};
-
-fn build_matches()->ArgMatches{
-    App::new("DotDot")
-        .version("0.1.0")
-        .author("iov billow.fun@gmail.com")
-        .about("Backup dot files")
-        .arg(Arg::with_name("config")
-            .short("c")
-            .long("config")
-            .value_name("FILE")
-            .help("Sets a custom config file")
-            .takes_value(true))
-        .arg(Arg::with_name("backup")
-            .short("b")
-            .help("Backup flag")
-            .index(1))
-        .arg(Arg::with_name("restore")
-            .short("r")
-            .help("Restore dot files from a directory")
-            .index(1)
-            .takes_value(true))
-        .arg(Arg::with_name("v")
-            .short("v")
-            .multiple(true)
-            .help("Sets the level of verbosity"))
-        .get_matches()
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let matches = build_matches();
+    let matches = App::new("DotDot")
+        .version("0.1.0")
+        .author("iov billow.fun@gmail.com")
+        .about("Backup dotfiles")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("backup")
+                .short("b")
+                .long("backup")
+                .help("Backup flag"),
+        )
+        .arg(
+            Arg::with_name("restore")
+                .short("r")
+                .long("restore")
+                .value_name("Directory".to_uppercase().as_str())
+                .help("Restore dotfiles from a directory")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .multiple(true)
+                .help("Sets the level of verbosity"),
+        )
+        .get_matches();
     Ok(())
 }
